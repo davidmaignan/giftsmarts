16c48d1318143a50889d4fcaa4aa760070b44c8b
David Maignan <davidmaignan@gmail.com>
2016-02-10 11:49:45 -0500

    update install script to add facebook-sdk manually coz no pip version for python 3

--- install-dev.sh
+++ install-dev.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 if ! hash grunt 2>/dev/null; then
     npm install -g grunt@0.1.13
@@ -15,3 +15,10 @@ gem install sass --verbose
 virtualenv env -p `which python3.4` --verbose
 
 env/bin/pip install -r requirements.txt --verbose
+
+# Manual install of facebook-sdk - No pip version only for python 2
+git clone https://github.com/pythonforfacebook/facebook-sdk.git
+cd facebook-sdk
+../env/bin/python3.4 setup.py install
+cd ..
+rm -rf facebook-sdk
--- install.sh
+++ install.sh
@@ -3,3 +3,11 @@
 virtualenv env -p `which python3.4` --verbose
 
 env/bin/pip install -r requirements.txt --verbose
+
+
+# Manual install of facebook-sdk - No pip version only for python 2
+git clone https://github.com/pythonforfacebook/facebook-sdk.git
+cd facebook-sdk
+../env/bin/python3.4 setup.py install
+cd ..
+rm -rf facebook-sdk
