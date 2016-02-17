d402d73ae14665cba7667be50d4d2358d53c4950
David Maignan <davidmaignan@gmail.com>
2016-02-17 17:02:57 -0500

    Add create method to post Actions

--- app/models/post.py
+++ app/models/post.py
@@ -1,7 +1,5 @@
+import arrow
 from app.config.config import db
-from app.config.config import app
-from app.constants import *
-from sqlalchemy.orm import relationship
 
 
 class Post(db.Model):
@@ -11,3 +9,23 @@ class Post(db.Model):
     created = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.String, db.ForeignKey('user.id'))
 
+
+class PostActions:
+    model = Post
+
+    @classmethod
+    def create(cls, post, user):
+        try:
+            created = arrow.get(post['created_time']).datetime
+            story = post['story'] if ('story' in post.keys()) else post['message']
+
+            new_post = cls.model(id=post['id'],
+                                 story=story,
+                                 created=created,
+                                 user_id=user['id']
+                                 )
+            db.session.add(new_post)
+            db.session.commit()
+            return new_post
+        except Exception:
+            return None
