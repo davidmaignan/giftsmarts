3e27f1e4f69b62d82357a40ea029c09b338cc4ae
David Maignan <davidmaignan@gmail.com>
2016-02-19 13:59:42 -0500

    Update run script with celery and redis. Set scripts to background and log in files debug/error messages

--- run-dev.sh
+++ run-dev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 PID=$(pgrep -f "runserver|celery|redis")
-for i in $PID;
+for i in ${PID};
 do
-    echo $i
-    kill $i
+    echo ${i}
+    kill ${i}
 done
 
 export FLASK_ENV="dev"
 
-env/bin/python manage.py runserver -d -r > logs/server.log 2>&1
-env/bin/celery -A app.config.config.celery worker --loglevel=debug > logs/celery.log 2>&1 &
-redis/src/redis-server 2>&1 > logs/redis.log &
+redis/src/redis-server > logs/redis.log 2>&1 &
+env/bin/python manage.py runserver -d -r > logs/server.log 2>&1 &
+env/bin/celery -A app.config.config.celery worker --loglevel=debug > logs/celery.log 2>&1  &
