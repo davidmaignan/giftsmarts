4dc087bd10f4bd1d54657f04f113406fc2a30e53
philtrep <phil.trepanier@gmail.com>
2016-03-09 15:43:48 -0500

    Small fixes for setup

--- app/config/config.py
+++ app/config/config.py
@@ -26,7 +26,7 @@ else:
 with open(config_path, 'r') as stream:
     parsed_config = yaml.load(stream)
 
-
+config["STOPLIST"] = 'data/stoplist.txt'
 config["JWT_ALGORITHM"] = 'HS512'
 config["BCRYPT_ROUNDS"] = 12
 #session duration in minutes
