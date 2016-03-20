75d2c97d86902f10510a718e3bb9c594a21d652d
johnlupien <lupien-gougeon.jonathan@courrier.uqam.ca>
2016-03-20 13:35:11 -0400

    Correction d'erreurs et modification au nom du model

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,11 +1,11 @@
-mport arrow
+import arrow
 import datetime
 import pprint
 from app.config.config import db
 
 
-class Contact(db.Model):
-    __tablename__ = 'contact'
+class Comment(db.Model):
+    __tablename__ = 'comment'
     id = db.Column(db.String, primary_key=True)
     user_id = db.Column(db.String, db.ForeignKey('user.id'))
     feedback = db.Column(db.String, nullable=False)
@@ -14,28 +14,28 @@ class Contact(db.Model):
 
 
 class ContactActions:
-    model = Contact
+    model =Comment
 
     @classmethod
-    def create(cls, contact, user):
+    def create(cls, comment, user):
         try:
-            created = arrow.get(contact['created_time']).datetime
-            story = contact['feedback'] if ('contact' in post.keys()) else post['message']
+            created = arrow.get(comment['created_time']).datetime
+            story = comment['feedback'] if ('comment' in post.keys()) else post['message']
 
-            new_feedback = cls.model(id=contact['id'],
+            new_comment = cls.model(id=comment['id'],
                                  user_id=user['id'],
                                  feedback=feedback,
                                  created=created,
                                  is_contacted=0
                                  )
-            db.session.add(new_feedback)
+            db.session.add(new_comment)
             db.session.commit()
-            return new_feedback
+            return new_comment
         except Exception:
             return None
 
     @classmethod
     def create_from_csv(cls, row):
-        contact = Contact(id=row[0], user_id=row[1], feedback=row[2], created=arrow.get(row[3]).datetime, is_contacted=row[4])
-        db.session.add(contact)
+        comment = Comment(id=row[0], user_id=row[1], feedback=row[2], created=arrow.get(row[3]).datetime, is_contacted=row[4])
+        db.session.add(comment)
         db.session.commit()
