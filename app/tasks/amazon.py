e3f409b36b1a5b2fc8ad005c2071955d897acd6d
David Maignan <davidmaignan@gmail.com>
2016-03-30 18:19:54 -0400

    remove commented code

--- app/tasks/amazon.py
+++ app/tasks/amazon.py
@@ -5,20 +5,6 @@ from app.models.amazon import UserProductActions, ProductActions
 from app.models.category import CategoryActions
 
 
-# @celery.task
-# def get_product(user):
-#     products = amazon.search(Keywords='Star Wars', SearchIndex='Books')
-#
-#     category = CategoryActions.find_by_name("Books")
-#
-#     for product in products:
-#         product_endity = ProductActions.create(product.asin)
-#         print(product_endity)
-#         redis.set(product.asin, product.to_string())
-#         UserProductActions.create(user, product_endity, category)
-#     pass
-
-
 @celery.task(bind=True)
 def get_product(self, user):
     products = amazon.search(Keywords='Star Wars', SearchIndex='Books')
