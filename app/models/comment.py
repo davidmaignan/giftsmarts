29ab846dbfbba8331af7f7d9b0fd384369464d40
johnlupien <lupien-gougeon.jonathan@courrier.uqam.ca>
2016-04-09 20:49:15 -0400

    comment.py cleanup - Removed global variable - renamed comment_id to id - deleted create_2() and create_from_cvs

--- app/models/comment.py
+++ app/models/comment.py
@@ -4,15 +4,10 @@ import pprint
 from app.config.config import db
 from flask import g
 
-i = 0
-def mydefault():
-    global i
-    i += 1
-    return i
 
 class Comment(db.Model):
     __tablename__ = 'comment'
-    comment_id = db.Column(db.String, primary_key=True, default=mydefault)
+    id = db.Column(db.Integer, primary_key=True, default=mydefault)
     user_id = db.Column(db.String, db.ForeignKey('user.id'))
     subject = db.Column(db.String, nullable=False)
     feedback = db.Column(db.String, nullable=False)
@@ -36,25 +31,3 @@ class CommentActions:
             return new_comment
         except Exception:
             return None
-
-    @classmethod
-    def create_2(cls, comment, user):
-        try:
-            new_comment = cls.model(user_id=user,
-                                    subject=comment['subject'],
-                                    feedback=comment['feedback'],
-                                    created=db.func.current_timestamp(),
-                                    is_contacted=0
-                                    )
-            db.session.add(new_comment)
-            db.session.commit()
-            return new_comment
-        except Exception:
-            return None
-
-
-    @classmethod
-    def create_from_csv(cls, row):
-        comment = Comment(id=row[0], user_id=row[1], feedback=row[2], created=arrow.get(row[3]).datetime, is_contacted=row[4])
-        db.session.add(comment)
-        db.session.commit()
